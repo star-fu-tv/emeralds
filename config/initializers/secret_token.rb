45808d8e152d073d7a5101d1242330099f976416
star-fu-tv <nia.radical@gmail.com>
2014-03-06 23:06:01 +0000

    First ride on rails...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emeralds::Application.config.secret_key_base = 'd40eb190c64cd7fc5095f16f9ee0e3a1fce65de1f521b43d86ed04e2cd67b8b8f0a7a6319f961f2877a802de54dc4c77280b302eb3692c354b2883c424420bb2'
