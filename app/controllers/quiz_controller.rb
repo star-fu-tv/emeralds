654bea151f4bc0075a12866015454775cca5ec09
star-fu-tv <nia.radical@gmail.com>
2014-03-08 12:57:25 +0000

    move pusher greeting to quiz controller

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,7 +1,9 @@
 class QuizController < ApplicationController
   def join
   	@message = "Hello, how are you?"
-  	
+	Pusher['test_channel'].trigger('greet', {
+  		:greeting => "Hello there!"
+	})
   end
 
   def question
